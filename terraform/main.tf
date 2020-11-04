de28b3a26191a48eec0e3984ee06ce93099c3324
sspyataev <pyataev.sergey@outlook.com>
2020-11-04 19:39:18 +0400

    refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,9 @@ terraform {
 
 # GKE cluster
 resource "google_container_cluster" "primary" {
-  name     = "${var.project_id}-gke"
-  location = var.region
+  name     = "project-gke"
+  location = "europe-north1"
+  cluster = google_container_cluster.primary.name
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -34,7 +35,7 @@ resource "google_container_cluster" "primary" {
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}-node-pool"
-  location   = var.region
+  location   = "europe-north1"
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
 
@@ -43,10 +44,6 @@ resource "google_container_node_pool" "primary_nodes" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
-
-    labels = {
-      env = var.project_id
-    }
 
     # preemptible  = true
     machine_type = "n1-standard-1"
